3f003a1b4814db5d65f8059e7f13c4662e2ac580
Wilfred Mutwiri <Wilfred Mutwiri>
2023-11-19 15:49:15 +0300

    add service section

--- src/script.js
+++ src/script.js
@@ -23,7 +23,22 @@ document.addEventListener('DOMContentLoaded', function () {
     updateText();
     setInterval(updateText,4000);
 });
+window.addEventListener("scroll",reveal);
+function reveal(){
+    var reveals=document.querySelectorAll("#reveal");
+    for(var i=0;i<reveals.length;i++){
 
+        var windowHeight=window.innerHeight;
+        var revealTop=reveals[i].getBoundingClientRect().top;
+        var revealPoint=200;
+
+        if(revealTop<windowHeight-revealPoint){
+            reveals[i].classList.add("active");
+        }else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
 
   
 
