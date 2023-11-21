82cb003165e727baed602ce3aa8cd0355cfa2b0a
Wilfred Mutwiri <Wilfred Mutwiri>
2023-11-22 00:12:51 +0300

    add more about info

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ menuIcon.addEventListener("click",()=>{
 });
 // home text changer
 document.addEventListener('DOMContentLoaded', function () {
-    const texts = ['Unleash the Possible: Join Our Journey in Community Empowerment and Youth Advancement!','Harmony in Action: Bridging Generations, Transforming Lives in Our Shared Community','Building Bridges, Breaking Barriers: Unleashing the Potential of Every Youth in Our Community','Igniting Change, Inspiring Futures: A Community Hub for Youth Empowerment and Growth','Empowering Tomorrow: Building Stronger Communities, Transforming Young Lives.'];
+    const texts = ['Unleash the Possible: Join Our Journey in Community Empowerment and Youth Advancement!','Harmony in Action: Bridging Generations, Transforming Lives in Our Shared Community','An empowered community with the understanding of utilizing resources rationally for living dignified lives.','Igniting Change, Inspiring Futures: A Community Hub for Youth Empowerment and Growth','Empowering Tomorrow: Building Stronger Communities, Transforming Young Lives.'];
     let currentIndex = 0;
     const textContainer = document.getElementById('text-container');
     function updateText() {
